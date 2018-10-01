6809696622c93873e56b5de794c20f65a7905d53
e-barr <github email address>
2018-10-01 11:49:02 -0500

    add login feature

---
+++ src/components/Login/login.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      username: '',
+      password: '',
+      response: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  login = event => {
+    event.preventDefault()
+
+    fetch("http://localhost:3000/api/v1/login", {
+      method: "POST",
+      body: JSON.stringify({
+        user: {
+          username: this.state.username,
+          password: this.state.password
+        }
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(resp => resp.json()).then(resp => {
+      if (resp.user) {
+        localStorage.token = resp.jwt
+        this.props.history.push("/")
+      }
+    })
+  }
+
+  render() {
+    return (
+      <form className="login-form" onSubmit={this.login}>
+        <label>Username</label>
+        <input type="text" name="username" onChange={this.handleChange} />
+        <label>Password</label>
+        <input type="password" name="password" onChange={this.handleChange}/>
+
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
+export default Login
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,19 +5,21 @@ import { Route, Switch } from 'react-router-dom';
 // Components
 import App from './components/App';
 import About from './components/About';
-import Coins from './components/Coins';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Notes from './components/Notes';
 import Todo from './components/Todo';
 import Error404 from './components/Error/404';
+import Login from './components/Login/login';
+import Signup from './components/Signup/signup';
 
 const AppRoutes = () => (
   <App>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} exact />
-      <Route path="/coins" component={Coins} exact />
+      <Route path="/login" component={Login} exact />
+      <Route path="/signup" component={Signup} exact />
       <Route path="/contact" component={Contact} exact />
       <Route path="/notes" component={Notes} exact />
       <Route path="/notes/:noteId" component={Notes} exact />
